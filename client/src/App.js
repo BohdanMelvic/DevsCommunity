371c3a3af425436bf06301d216fde50bd8cef751
Bohdan Melvic <bohdanmelvic@gmail.com>
2020-02-02 23:53:50 +0200

    router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Landing } from './components/layout/Landing';
 import { Navbar } from './components/layout/Navbar';
+import { Login } from './components/auth/Login';
+import { Register } from './components/auth/Register';
 
 
 const App = () => {
   return (
+   <Router>
     <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+   </Router>
   );
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
@@ -7,9 +8,9 @@ export const Navbar = () => {
                 <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
             </h1>
             <ul>
-                <li><a href="profiles.html">Developers</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="login.html">Login</a></li>
+                <li><Link to="/">Developers</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/login">Login</Link></li>
             </ul>
         </nav>
     )
