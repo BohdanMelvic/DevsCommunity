fdb444c7d8e8962766e26c0328666c7fcbd858a5
Bohdan Melvic <bohdanmelvic@gmail.com>
2020-02-11 23:26:28 +0200

    added ProfileTop ProfileAbout Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { getProfileById } from '../../store/actions/profileActions';
 import Spinner from '../Spinner/Spinner';
 import { Link } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({
     getProfileById, 
@@ -23,6 +25,11 @@ const Profile = ({
                 </Link>
                 { auth.isAuthenticated && auth.loading === false && auth.user._id ===
                     profile.user._id && (<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
+
+                    <div className="profile-grid my-1">
+                        <ProfileTop profile={profile} />
+                        <ProfileAbout profile={profile} />
+                    </div>
             </Fragment>}
         </Fragment>
     )
